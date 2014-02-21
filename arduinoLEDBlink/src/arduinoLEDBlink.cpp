4d39e3fcb948a385bbe39b2dc2f8a9cd0372205f
Daniel Holmlund <holmlund.dev@gmail.com>
2014-02-20 17:00:07 -0800

    Update arduinoLEDBlink.cpp

--- arduinoLEDBlink/src/arduinoLEDBlink.cpp
+++ arduinoLEDBlink/src/arduinoLEDBlink.cpp
@@ -1,4 +1,5 @@
 #line 1 "Blink.ino"
+#include "Arduino.h"
 /*
   Blink
   Turns on an LED on for one second, then off for one second, repeatedly.
@@ -8,38 +9,18 @@
  
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
-#include "Arduino.h"
-void setup();
-void loop();
-#line 10
-int led = 9;
-int led2 = 8;
-int led3 = 7;
+int led = 13;
 
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
+  pinMode(led, OUTPUT);     
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(200);               // wait for a second
+  delay(1000);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(200);               // wait for a second
-
-  digitalWrite(led2, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(200);               // wait for a second
-  digitalWrite(led2, LOW);    // turn the LED off by making the voltage LOW
-  delay(200);               // wait for a second
-
-  digitalWrite(led3, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(200);               // wait for a second
-  digitalWrite(led3, LOW);    // turn the LED off by making the voltage LOW
-  delay(200);               // wait for a second
-
+  delay(1000);               // wait for a second
 }
-
