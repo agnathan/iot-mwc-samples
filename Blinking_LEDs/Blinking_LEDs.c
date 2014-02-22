51ee76ad227a067d4063458f2105a019a37e1b98
Sulamita Garcia <sulamita@localhost.localdomain>
2014-02-22 08:42:36 +0100

    Fixing license

--- Blinking_LEDs/Blinking_LEDs.c
+++ Blinking_LEDs/Blinking_LEDs.c
@@ -1,5 +1,11 @@
 /*
- Copyright (c) 2014 Intel Corporation.
+ Copyright (c) 2014 Intel Corporation  All Rights Reserved.
+ The source code, information and material ("Material") contained herein is owned by Intel Corporation or its suppliers or licensors, and title to such Material remains with Intel Corporation 
+ or its suppliers or licensors. The Material contains proprietary information of Intel or its suppliers and licensors. The Material is protected by worldwide copyright laws and treaty provisions. 
+ No part of the Material may be used, copied, reproduced, modified, published, uploaded, posted, transmitted, distributed or disclosed in any way without Intel's prior express written permission. 
+ No license under any patent, copyright or other intellectual property rights in the Material is granted to or conferred upon you, either expressly, by implication, inducement, estoppel or otherwise. 
+ Any license under such intellectual property rights must be express and approved by Intel in writing.
+
 *
 * Author: Sulamita Garcia
 * Based on LEDblink.c by Nandkishor Sonar.
@@ -11,16 +17,21 @@
 * copies of the Software, and to permit persons to whom the Software is
 * furnished to do so, subject to the following conditions:
 *
- * The above copyright notice and this permission notice shall be included in
+* The above copyright notice and this permission notice shall be included in
 * all copies or substantial portions of the Software.
 *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 * THE SOFTWARE.
+
+Include supplier trademarks or logos as supplier requires Intel to use, preceded by an asterisk. An asterisked footnote can be added as follows: *Third Party trademarks are the property of their respective owners.
+
+Unless otherwise agreed by Intel in writing, you may not remove or alter this notice or any other notice embedded in Materials by Intel or Intel’s suppliers or licensors in any way.”
+
 */
 
 /* This code blinks LEDs connected to Galileo GPIO3 (Arduino PIN13), GPIO17,
